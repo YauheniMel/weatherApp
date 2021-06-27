aa53a360b1f01039fe1697e178bc42a6f7de5b5e
EkaterinaLapunova <katyalapunova96@gmail.com>
2021-06-27 16:52:32 +0300

    Install Eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
+  context: path.resolve(__dirname, 'src'),
   entry: {
-    index: "./js/index.js"
+    index: './js/index.js',
   },
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist")
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: "./index.html"
+      template: './index.html',
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
@@ -25,4 +25,4 @@ module.exports = {
       },
     ],
   },
-}
+};
