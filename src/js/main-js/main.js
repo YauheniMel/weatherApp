48197ef591a1fb546932466a6a4b548388a219be
EkaterinaLapunova <katyalapunova96@gmail.com>
2021-07-01 00:55:55 +0300

    Add next-days-data.js

--- src/js/main-js/main.js
+++ src/js/main-js/main.js
@@ -1,11 +1,14 @@
 import fetchData from '../fetch-data';
+import NextDaysData from './next-days-data';
 
 const key = '03fb54ebf904aeecf7fbb0e169f0c7ad';
 const urlWeather5 = `https://api.openweathermap.org/data/2.5/forecast?q=Minsk&appid=${key}`;
 
 fetchData('GET', urlWeather5)
   .then((response) => {
-    // console.log(response);
+    const data = JSON.parse(response);
+
+    new NextDaysData(data);
   })
   .catch((error) => {
     console.error(error);
---
+++ src/js/main-js/next-days-data.js
@@ -0,0 +1,13 @@
+class NextDaysData {
+  constructor(data) {
+    this.data = data;
+
+    this.init();
+  }
+
+  init() {
+    console.log(this.data);
+  }
+}
+
+export default NextDaysData;
