e0b6c0ff0cb6a7f0fddf07df1b47218a82b2d978
EkaterinaLapunova <katyalapunova96@gmail.com>
2021-07-03 00:12:32 +0300

    Add render-main-element.js

--- src/js/main-js/render-main-element.js
+++ src/js/main-js/render-main-element.js
@@ -1,7 +1,20 @@
 function renderMainElement(arrTemp, arrIcon, arrDate) {
-  console.log(arrTemp, arrIcon, arrDate);
+  const daysWrapEl = document.querySelector('.days');
+
   for (let i = 0; i < 5; i++) {
-    console.log(i);
+    const template = `
+      <div class="day">
+        <p class="day__temperature">
+          ${arrTemp[i]}
+        </p>
+        <img src="http://openweathermap.org/img/wn/${arrIcon[i]}@2x.png" alt="" class="day__icon">
+        <p class="day__value">
+          ${arrDate[i]}
+        </p>
+      </div>
+    `;
+
+    daysWrapEl.innerHTML += template;
   }
 }
 
